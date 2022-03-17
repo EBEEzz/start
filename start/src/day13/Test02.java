39b9ca3819f8bc4cce8eb127c9c40c56a744f1c5
AnEunbee <AnEunbee@DESKTOP-8CBASVO>
2022-03-17 09:37:19 +0900

    day13

---
+++ start/src/day13/Test02.java
@@ -0,0 +1,7 @@
+package day13;
+
+public class Test02 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ start/src/day13/Test09.java
@@ -0,0 +1,10 @@
+package day13;
+
+public class Test09 {
+	public Test09() {
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
